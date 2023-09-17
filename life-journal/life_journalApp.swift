7ed28e19f7fb99b7e32454a0d9aa526c7d8048a9
Jimmy DeLano <jimmydelano91@gmail.com>
2023-09-17 11:16:39 -0700

    Initial Commit

---
+++ life-journal/life_journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  life_journalApp.swift
+//  life-journal
+//
+//  Created by Jimmy DeLano on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct life_journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
